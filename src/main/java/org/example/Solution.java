76d0880b75cbb6e87fcc8cc78d7125818da42183
annt <annt@gft.com>
2022-11-24 16:53:07 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int maximumGap(int[] nums) {
+
+        int gaps = 0;
+        int aux = 0;
+
+        if (nums.length < 2){
+
+            return gaps;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++){
+
+           aux = nums[i+1] - nums[i];
+
+           if (gaps < aux){
+
+               gaps = aux;
+           }
+
+        }
+
+        return gaps;
+
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,47 @@
-package org.example;public class SOlutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+    int[] array;
+    int result;
+
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void arrayContainsOnlyOneNumber(){
+
+        array = new int[] {4};
+        result = 0;
+
+        assertEquals(result, solution.maximumGap(array));
+    }
+
+    @Test
+    public void arrayContainsTwoNumbers(){
+
+        array = new int[] {4,1};
+        result = 3;
+
+        assertEquals(result, solution.maximumGap(array));
+    }
+
+    @Test
+    public void arrayContainsFiveNumbers(){
+
+        array = new int[] {4,1,5,6,22};
+        result = 16;
+
+        assertEquals(result, solution.maximumGap(array));
+    }
 }
